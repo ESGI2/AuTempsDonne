c742e02577b939196629bb65c3cadd8586b80205
Sitron808 <kevinsureshe@gmail.com>
2024-02-16 01:28:49 +0100

    Route with Middleware + check Admin

--- api/routes/event.route.js
+++ api/routes/event.route.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const eventController =  require('../controllers/event.controller');
 const router =  express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const isAdmin = require('../middlewares/isAdmin');
 
-router.get('/', eventController.getAllEvents);
-router.get('/:id', eventController.getEventById);
-router.post('/', eventController.addEvent);
-router.put('/:id', eventController.updateEvent);
-router.delete('/:id', eventController.deleteEvent);
+router.get('/',[authMiddleware, isAdmin], eventController.getAllEvents);
+router.get('/:id',[authMiddleware, isAdmin], eventController.getEventById);
+router.post('/',[authMiddleware, isAdmin], eventController.addEvent);
+router.put('/:id',[authMiddleware, isAdmin], eventController.updateEvent);
+router.delete('/:id',[authMiddleware, isAdmin], eventController.deleteEvent);
 
+                        //Verif si il est responsable\\
 
 module.exports = router;
