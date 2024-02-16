ca7c0c215ddb1a235082f1fbaf71bade437b0a53
Sitron808 <kevinsureshe@gmail.com>
2024-02-16 03:12:16 +0100

    Activity Finish

--- api/controllers/event.controller.js
+++ api/controllers/event.controller.js
@@ -1,10 +1,9 @@
-const EventService = require ('../services/event.service');
-//const ActivityService = require('../services/activity.service');
-const moment = require('moment-timezone'); //Need 'npm install moment'
-const timeZone = 'Europe/Paris';
+const EventService = require('../services/event.service');
+const ActivityService = require('../services/activity.service');
+const moment = require('moment-timezone');
 
 
-class EventController{
+class EventController {
     //GET ALL & By ID
     static async getAllEvents(req, res) {
         try {
@@ -15,39 +14,41 @@ class EventController{
             res.status(500).json({"Error": "Error recovering event"});
         }
     }
-    static async getEventById(req, res){
+
+    static async getEventById(req, res) {
         try {
             const {id} = req.params;
             const event = await EventService.getEventById(id);
-            if(!event){
-                return res.status(404).json({"Error":"Event not found"});
-            }else{
+            if (!event) {
+                return res.status(404).json({"Error": "Event not found"});
+            } else {
                 return res.status(200).json(event);
             }
-        }catch (error){
+        } catch (error) {
             console.error(error);
             res.status(500).json({"Error": "Error recovering event"});
         }
     }
+
     //ADD
-    static async addEvent(req, res){
+    static async addEvent(req, res) {
         try {
-            const { name, description, start, end, activity_id } = req.body;
+            const {name, description, start, end, activity_id} = req.body;
             const requiredFields = ['name', 'description', 'start', 'end', 'activity_id'];
             let missingFields = [];
             const dateFormat = "DD/MM/YYYY HH:mm"; //Verify hh:mm => hours
 
-            requiredFields.forEach(field =>{
-                if (!req.body[field]){
+            requiredFields.forEach(field => {
+                if (!req.body[field]) {
                     missingFields.push(field);
                 }
             });
-            if (missingFields.length > 0){
+            if (missingFields.length > 0) {
                 return res.status(400).json({error: "Missing fields :", missingFields});
             }
 
             if (name.length > 50 || description.length > 255) {
-                return res.status(400).json({ error: "Name or description too long." });
+                return res.status(400).json({error: "Name or description too long."});
             }
 
             if (!moment(start, dateFormat, true).isValid() || !moment(end, dateFormat, true).isValid()) {
@@ -67,33 +68,32 @@ class EventController{
             if (moment(formattedStartDate).isAfter(formattedEndDate)) {
                 return res.status(400).json({error: "The start date must be before the end date."});
             }
-            //Refaire activityService
-           // const activity = await ActivityService.findById(activity_id);
-            // if (!activity) {
-            //    return res.status(400).json({ error: "Activity not found." });
-            //}
-            //Type activity need more precision,what's exactly the mean of it. ==> SUREMENT DEGAGE
+            const activity = await ActivityService.getActivityById(activity_id);
+            if (!activity) {
+                return res.status(400).json({error: "Activity not found."});
+            }
 
-            const eventData = { name, description, start: formattedStartDate, end: formattedEndDate, activity_id};
+            const eventData = {name, description, start: formattedStartDate, end: formattedEndDate, activity_id};
 
             const newEvent = await EventService.addEvent(eventData);
             res.status(201).json(newEvent);
-        }catch (error){
+        } catch (error) {
             console.error(error);
             res.status(500).json({error: "Error creating event"})
         }
     }
+
     //UPDATE
     static async updateEvent(req, res) {
         try {
             const {id} = req.params;
-            const { name, description, start, end, activity_id } = req.body;
+            const {name, description, start, end, activity_id} = req.body;
             const eventToUpdate = await EventService.getEventById(id);
             const updatedData = {};
             const dateFormat = "DD/MM/YYYY HH:mm";
 
             if (!eventToUpdate) {
-                return res.status(404).json({ error: "Event not found." });
+                return res.status(404).json({error: "Event not found."});
             }
 
             if (name) updatedData.name = name;
@@ -108,27 +108,28 @@ class EventController{
             }
 
             if (start && end && moment(updatedData.start).isAfter(updatedData.end)) {
-                return res.status(400).json({ error: "The start date must be before the end date." });
+                return res.status(400).json({error: "The start date must be before the end date."});
             }
 
             await eventToUpdate.update(updatedData);
-            res.status(200).json({message: "Event updated successfully", event: eventToUpdate });
+            res.status(200).json({message: "Event updated successfully", event: eventToUpdate});
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: "Error updating event" });
+            res.status(500).json({error: "Error updating event"});
         }
     }
+
     //DELETE
-    static async deleteEvent(req, res){
+    static async deleteEvent(req, res) {
         try {
             const {id} = req.params;
             const deleteEvent = await EventService.deleteEvent(id);
-            if (!deleteEvent){
+            if (!deleteEvent) {
                 res.status(404).json({error: "Event not found"});
-            }else {
+            } else {
                 res.status(200).json({message: "Event deleted success", deleteEvent});
             }
-        }catch (error){
+        } catch (error) {
             console.error(error);
             res.status(500).json({error: "Error deleting event"})
         }
