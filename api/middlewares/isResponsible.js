3bc1870b29eead4dc73f88bb903372016e276360
Enzo <enzo.moy94@gmail.com>
2024-02-21 10:19:22 +0100

    add new permissions middleware and update the others

---
+++ api/middlewares/isResponsible.js
@@ -0,0 +1,9 @@
+const checkIsResponsible = (req, res, next) => {
+    const userData = req.user;
+    if (userData.role !== "responsible" || userData.role !== "admin") {
+        return res.status(403).json({"Error": "You are not authorized to access this route"});
+    }
+    next();
+};
+
+module.exports = checkIsResponsible();
