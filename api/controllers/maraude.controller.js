536a40c4f9847c2b8e5f29030855454b68e1ace5
Sitron808 <123543937+Sitron808@users.noreply.github.com>
2024-02-23 11:17:52 +0100

    Update Maraude with all verification

--- api/controllers/maraude.controller.js
+++ api/controllers/maraude.controller.js
@@ -79,21 +79,50 @@ class MaraudeController{
     }
 
     //UPDATE
-    static async updateMaraude(req, res){
-        const { id } = req.params;
-        const maraudeData = req.body;
-        try{
-            const updateMaraude = await MaraudeService.updateMaraude(id, maraudeData);
-            if (!updateMaraude){
-                res.status(404).json({"Error":"Maraude not found"});
-            }else {
-                res.status(200).json(updateMaraude);
+    static async updateMaraude(req, res) {
+        try {
+            const {id} = req.params;
+            const {date, duration, id_truck, people_needed} = req.body;
+            const maraudeToUpdate = await MaraudeService.getMaraudeById(id);
+            const updatedData = {};
+            const dateFormat = "DD/MM/YYYY";
+            const durationFormat = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+
+            if (!maraudeToUpdate) {
+                return res.status(404).json({error: "Maraude not found."});
             }
-        }catch (error){
+
+            if (date && moment(date, dateFormat, true).isValid()) {
+                const inputDate = moment(date, dateFormat);
+                const currentDate = moment().startOf('day');
+                if (inputDate.isBefore(currentDate)) {
+                    return res.status(400).json({error: "Date cannot be in the past"});
+                }
+                updatedData.date = moment.utc(date, dateFormat).local().format('YYYY-MM-DD');
+            }
+
+            if (duration && durationFormat.test(duration)) {
+                updatedData.duration = duration;
+            } else if (duration) {
+                return res.status(400).json({error: "Invalid duration format. Use HH:mm"});
+            }
+
+            if (id_truck) updatedData.id_truck = id_truck;
+            if (people_needed) {
+                if (people_needed <= 0) {
+                    return res.status(400).json({error: "Invalid number of people needed. Must be greater than 0"});
+                }
+                updatedData.people_needed = people_needed;
+            }
+
+            await MaraudeService.updateMaraude(id, updatedData);
+            res.status(200).json({message: "Maraude updated successfully", maraude: updatedData});
+        } catch (error) {
             console.error(error);
-            res.status(500).json({"Error":"Error updating maraude"});
+            res.status(500).json({error: "Error updating maraude"});
         }
     }
+
 
     //DELETE
     static async deleteMaraude(req, res){
