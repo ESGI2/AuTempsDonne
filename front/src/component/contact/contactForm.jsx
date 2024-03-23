5fc12789c23069a74f48d966c7cf18c06e2336be
Sitron808 <123543937+Sitron808@users.noreply.github.com>
2024-03-23 09:43:26 +0100

    WIP need link to social média and images

--- front/src/component/contact/contactForm.jsx
+++ front/src/component/contact/contactForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import './contactForm.css'
 import {Link} from "react-router-dom";
 import {beforeRead} from "@popperjs/core";
@@ -14,7 +14,7 @@ function Contact() {
     });
 
     const handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
+        const {name, value, type, checked} = e.target;
         setFormData(prevState => ({
             ...prevState,
             [name]: type === 'checkbox' ? checked : value
@@ -27,16 +27,38 @@ function Contact() {
     };
 
     return (
-        <section  className="text-center" style={
-            {
-
-            }
+        <section className="text-center" style={
+            {}
         }>
+
+            <div className="icons-container">
+                <a style={{color: '#3b5998'}} href="#!" role="button">
+                    <i className="fab fa-facebook-f fa-lg"></i>
+                </a>
+
+                <a style={{color: '#55acee'}} href="#!" role="button">
+                    <i className="fab fa-twitter fa-lg"></i>
+                </a>
+
+                <a style={{color: '#dd4b39'}} href="#!" role="button">
+                    <i className="fab fa-google fa-lg"></i>
+                </a>
+
+                <a style={{color: '#ac2bac'}} href="#!" role="button">
+                    <i className="fab fa-instagram fa-lg"></i>
+                </a>
+            </div>
+
+
             <div className="p-5 bg-image" style={{
-                height: '300px',
+                height: '250px',
                 borderRadius: '20px',
 
-            }}></div>
+
+            }}>
+
+            </div>
+
 
             <div id='reg' className="card mx-4 mx-md-5 shadow-5-strong" style={{
                 marginTop: '-250px',
@@ -46,7 +68,8 @@ function Contact() {
                 <div className="card-body py-5 px-md-5">
                     <div className="row d-flex justify-content-center">
                         <div className="col-lg-8">
-                            <h1 className="fw-bold mb-5">Pour toutes autre demande, contactez-nous via le formulaire ci-dessous</h1>
+                            <h1 className="fw-bold mb-5">Pour toutes autre demande, contactez-nous via le formulaire
+                                ci-dessous</h1>
                             <form onSubmit={handleSubmit}>
 
                                 <div className="form-outline mb-4">
@@ -107,6 +130,68 @@ function Contact() {
                     </div>
                 </div>
             </div>
+            <br/><br/><br/>
+            <div className="card-group">
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" className="card-img-top"
+                         alt="Hollywood Sign on The Hill"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Moy Enzo</h5>
+                        <p className="card-text">
+                            Co-Founder of the association
+                        </p>
+                        <p className="card-text">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum.
+                        </p>
+
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top"
+                         alt="Palm Springs Road"/>
+                    <div className="card-body">
+                        <h5 className="card-title">SURESHE Kévin</h5>
+                        <p className="card-text">Founder of the association</p>
+                        <p className="card-text">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum.
+                        </p>
+
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" className="card-img-top"
+                         alt="Los Angeles Skyscrapers"/>
+                    <div className="card-body">
+                        <h5 className="card-title">ARFAOUI Karim</h5>
+                        <p className="card-text">
+                            Co-Founder of the association
+                        </p>
+                        <p className="card-text">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
         </section>
 
     );
