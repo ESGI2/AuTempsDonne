c1571e58fca9946b47d07a07dc15bbc080773c8c
Karimarf <karimaarfaouim@gmail.com>
2024-03-28 16:25:13 +0100

    Add language translation in contact page

--- front/src/component/contact/contactForm.jsx
+++ front/src/component/contact/contactForm.jsx
@@ -1,18 +1,23 @@
-import {useState} from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import lang_fr from '../../assets/lang/lang_fr.json';
+import lang_en from "../../assets/lang/lang_en.json";
 import './contactForm.css';
 
-function Contact() {
+function ContactForm() {
+    const { pathname } = useLocation();
+    const lang = pathname.startsWith('/en') ? lang_en : lang_fr;
+
     const [formData, setFormData] = useState({
         firstName: '',
         lastName: '',
         email: '',
-        password: '',
         message: '',
         subscribe: true,
     });
 
     const handleChange = (e) => {
-        const {name, value, type, checked} = e.target;
+        const { name, value, type, checked } = e.target;
         setFormData(prevState => ({
             ...prevState,
             [name]: type === 'checkbox' ? checked : value
@@ -25,42 +30,29 @@ function Contact() {
     };
 
     return (
-        <div >
-            <h1 className="text-center" style={
-                {paddingTop: '50px'}
-            }>Contact</h1>
+        <div>
+            <h1 className="text-center" style={{ paddingTop: '50px' }}>{lang.contactForm.titre}</h1>
 
-            <section className="text-center" style={
-                {}
-            }>
-
-                <div className="icons-container" style={
-                    {paddingBottom: '50px'}
-                }>
-                    <a style={{color: '#3b5998'}} href="#!" role="button">
+            <section className="text-center">
+                <div className="icons-container" style={{ paddingBottom: '50px' }}>
+                    <a style={{ color: '#3b5998' }} href="#!" role="button">
                         <i className="fab fa-facebook-f fa-lg"></i>
                     </a>
 
-                    <a style={{color: '#55acee'}} href="#!" role="button">
+                    <a style={{ color: '#55acee' }} href="#!" role="button">
                         <i className="fab fa-twitter fa-lg"></i>
                     </a>
 
-                    <a style={{color: '#dd4b39'}} href="#!" role="button">
+                    <a style={{ color: '#dd4b39' }} href="#!" role="button">
                         <i className="fab fa-google fa-lg"></i>
                     </a>
 
-                    <a style={{color: '#ac2bac'}} href="#!" role="button">
+                    <a style={{ color: '#ac2bac' }} href="#!" role="button">
                         <i className="fab fa-instagram fa-lg"></i>
                     </a>
                 </div>
 
-
-                <div className="p-5 bg-image" style={{
-                    height: '250px',
-                }}>
-
-                </div>
-
+                <div className="p-5 bg-image" style={{ height: '250px' }}></div>
 
                 <div id='reg' className="mx-4 mx-md-5 shadow-5-strong" style={{
                     marginTop: '-250px',
@@ -72,62 +64,52 @@ function Contact() {
                     <div className="card-body py-5 px-md-5">
                         <div className="row d-flex justify-content-center">
                             <div className="col-lg-8">
-                                <h1 className="fw-bold mb-5">Pour toutes autre demande, contactez-nous via le formulaire
-                                    ci-dessous</h1>
+                                <h1 className="fw-bold mb-5">{lang.contactForm.subtitre}</h1>
                                 <form onSubmit={handleSubmit}>
 
                                     <div className="form-outline mb-4">
-                                        <label htmlFor="inputState">Type of request</label>
+                                        <label htmlFor="inputState">{lang.contactForm.type_request}</label>
                                         <select id="inputState" className="form-control">
-                                            <option selected>I want to be a Beneficiary but ....</option>
-                                            <option selected>To do a Donation</option>
-                                            <option selected>Need Help</option>
-                                            <option selected>Nature Donation(Clothing, pants, ...)</option>
-                                            <option selected>Partnership</option>
-                                            <option selected>Choose ...</option>
+                                            {lang.contactForm.request_options.map((option, index) => (
+                                                <option key={index}>{option}</option>
+                                            ))}
                                         </select>
                                     </div>
 
                                     <div className="row">
                                         <div className="col-md-6 mb-4">
                                             <div className="form-outline">
-                                                <label className="form-label" htmlFor="form3Example1">First name</label>
-                                                <input type="text" id="form3Example1" name="firstName"
-                                                       className="form-control" onChange={handleChange}/>
+                                                <label className="form-label" htmlFor="form3Example1">{lang.contactForm.first_name}</label>
+                                                <input type="text" id="form3Example1" name="firstName" className="form-control" onChange={handleChange} />
                                             </div>
                                         </div>
                                         <div className="col-md-6 mb-4">
                                             <div className="form-outline">
-                                                <label className="form-label" htmlFor="form3Example2">Last name</label>
-                                                <input type="text" id="form3Example2" name="lastName"
-                                                       className="form-control" onChange={handleChange}/>
+                                                <label className="form-label" htmlFor="form3Example2">{lang.contactForm.last_name}</label>
+                                                <input type="text" id="form3Example2" name="lastName" className="form-control" onChange={handleChange} />
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="form-outline mb-4">
-                                        <label className="form-label" htmlFor="form3Example3">Email</label>
-                                        <input type="email" id="form3Example3" name="email" className="form-control"
-                                               onChange={handleChange}/>
+                                        <label className="form-label" htmlFor="form3Example3">{lang.contactForm.email}</label>
+                                        <input type="email" id="form3Example3" name="email" className="form-control" onChange={handleChange} />
                                     </div>
 
                                     <div>
-                                        <label htmlFor="form3Example4">Message</label>
-                                        <textarea className="form-control" id="form3Example4" rows="4" name="message"
-                                                  onChange={handleChange}></textarea>
+                                        <label htmlFor="form3Example4">{lang.contactForm.message}</label>
+                                        <textarea className="form-control" id="form3Example4" rows="4" name="message" onChange={handleChange}></textarea>
                                     </div>
 
                                     <div className="form-check d-flex justify-content-center mb-4">
-                                        <input className="form-check-input me-2" type="checkbox" value=""
-                                               id="form2Example33" name="subscribe" checked={formData.subscribe}
-                                               onChange={handleChange}/>
+                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" name="subscribe" checked={formData.subscribe} onChange={handleChange} />
                                         <label className="form-check-label" htmlFor="form2Example33">
-                                            Subscribe to our newsletter
+                                            {lang.contactForm.subscribe_newsletter}
                                         </label>
                                     </div>
 
                                     <button type="submit" className="btn btn-primary btn-block mb-4">
-                                        Submit
+                                        {lang.contactForm.submit_button}
                                     </button>
                                 </form>
                             </div>
@@ -136,8 +118,7 @@ function Contact() {
                 </div>
             </section>
         </div>
-
-);
+    );
 }
 
-export default Contact;
+export default ContactForm;
