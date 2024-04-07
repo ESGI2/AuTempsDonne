c6a135c816ec2d1797415f58530c3c6854b45a60
Sitron808 <123543937+Sitron808@users.noreply.github.com>
2024-04-07 09:16:19 +0200

    edit main page

--- front/src/component/main/main.jsx
+++ front/src/component/main/main.jsx
@@ -1,82 +1,67 @@
-import React from 'react';
-import banner from "../../assets/img/banner.png";
-import momkids from "../../assets/img/momkids.png";
-import asso1 from "../../assets/img/asso1.png";
-import asso2 from "../../assets/img/asso2.png";
-import asso3 from "../../assets/img/asso3.png";
-import asso4 from "../../assets/img/asso4.png";
-import {Link, useLocation} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import ky from 'ky';
+import { Link, useLocation } from "react-router-dom";
 import lang_fr from '../../assets/lang/lang_fr.json';
 import lang_en from "../../assets/lang/lang_en.json";
 
-
 function Main() {
-
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
     const lang = pathname.startsWith('/en') ? lang_en : lang_fr;
     const langParam = pathname.startsWith('/en') ? 'en' : 'fr';
 
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstName] = useState('');
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await ky.get('http://localhost:3000/user/me', {
+                    credentials: 'include',
+                }).json();
+                console.log(response);
+                const { first_name, last_name } = response.me;
+                setFirstName(first_name);
+                setLastName(last_name);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données de l\'utilisateur');
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
     return (
-        <>
-            <div style={{overflow: "auto"}}>
-                <div id="carouselExampleCaptions" className="carousel slide ml-auto mr-md-5">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={banner} className="d-block w-100" style={{maxHeight: "400px"}} alt="Banner"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={momkids} className="mt-5 rounded float-start" alt="Mom with kids"/>
-                        </div>
-                        <div className="col-md-6">
-                            <div id="Nouscoco" className="mt-lg-5">
-                                <h1 className="display-5"><b>{lang.home.nous_connaitre.titre}</b></h1>
-                                <p>{lang.home.nous_connaitre.paragraphe1}</p>
-                                <p>{lang.home.nous_connaitre.paragraphe2}</p>
-                                <p>{lang.home.nous_connaitre.paragraphe3}</p>
-                                <p>{lang.home.nous_connaitre.paragraphe4}</p>
-                                <Link to={`/${langParam}/mission`}>
-                                    <button className="button">
-                                        <p>{lang.home.nous_connaitre.bouton_missions}</p>
-                                    </button>
-                                </Link>
+        <div className="container mt-5">
+            <section className="text-center" style={{
+                paddingLeft: '20px',
+                paddingRight: '20px',
+                paddingBottom: '20px',
+            }}>
+                <div className="p-5 bg-image" style={{
+                    height: '250px',
+                }}></div>
+
+                <div id='reg' className="mx-4 mx-md-5 shadow-5-strong" style={{
+                    marginTop: '-250px',
+                    background: 'linear-gradient(rgba(91, 131, 166, 0.5), rgba(209, 209, 209, 0.5))',
+                    backdropFilter: 'blur(30px)',
+                    borderRadius: '20px',
+                    paddingBottom: '50px',
+                }}>
+                    <div className="card-body py-5 px-md-5">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-lg-8">
+                                <h1 className="fw-bold mb-5">{lang.main.title}</h1>
+                                <form>
+                                    <h1>{`${firstName} ${lastName}`}</h1>
+                                </form>
+                                <p className="text-center">{lang.main.subtitle}</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="container text-center mt-lg-5 bg-warning-subtle">
-                    <h1 className="display-5"><b>{lang.home.nos_partenaires}</b></h1>
-                    <div className="row row-cols-4">
-                        <div className="col order-first"><img src={asso1} alt="Association 1"/></div>
-                        <div className="col"><img src={asso2} alt="Association 2"/></div>
-                        <div className="col"><img src={asso3} alt="Association 3"/></div>
-                        <div className="col order-last"><img src={asso4} alt="Association 4"/></div>
-                    </div>
-                </div>
-                <div id="Nousreje" className="mt-lg-5 text-center mx-auto">
-                    <h1 className="display-3"><b>{lang.home.on_recrute.titre}</b></h1>
-                    <p>{lang.home.on_recrute.paragraphe1}</p>
-                    <p>{lang.home.on_recrute.paragraphe2}</p>
-                    <p>{lang.home.on_recrute.paragraphe3}</p>
-                    <p>{lang.home.on_recrute.paragraphe4}</p>
-                    <p>{lang.home.on_recrute.paragraphe5}</p>
-                    <Link to={`/${langParam}/login`}>
-                        <button className="button">
-                            <p>{lang.home.on_recrute.bouton_rejoindre}</p>
-                        </button>
-                    </Link>
-                </div>
-                <div className="container mt-5 mb-5">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.4252194424103!2d3.2663266768230304!3d49.85325663018251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e818f421f465bf%3A0x95ab756da5e0559a!2sAssociation%20MultiCit%C3%A9%20-%20Centre%20Social%20du%20Vermandois!5e0!3m2!1sfr!2sfr!4v1710741072140!5m2!1sfr!2sfr"
-                        width="100%" height="500" style={{border: "0"}} loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade" title="Google Maps"></iframe>
-                </div>
-            </div>
-        </>
+            </section>
+        </div>
     );
 }
 
--- front/src/logPages/Main.page.jsx
+++ front/src/logPages/Main.page.jsx
@@ -1,10 +1,12 @@
 import Footer from "../component/footer/footer.jsx";
 import LogHeader from "../component/header/logHeader.jsx";
+import Main from "../component/main/main.jsx";
 
 function MainLog() {
     return (
         <>
             <LogHeader/>
+                <Main/>
             <Footer/>
         </>
     );
