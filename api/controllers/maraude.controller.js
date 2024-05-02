ef931698ee93b0fc37b71fc37ee7e2e64990a47e
Enzo <enzo.moy94@gmail.com>
2024-05-02 14:09:06 +0200

    WIP - Changement API pour maraude

--- api/controllers/maraude.controller.js
+++ api/controllers/maraude.controller.js
@@ -45,44 +45,25 @@ class MaraudeController{
     //ADD
     static async addMaraude(req, res) {
         try {
-            const { date, duration, id_truck, people_needed } = req.body;
-            const requiredFields = ['date', 'duration', 'id_truck', 'people_needed'];
+            const { title, description, date_start, date_end, road_start, road_end, road_inter, truck } = req.body;
+            const requiredFields = ['title', 'description', 'date_start', 'date_end', 'road_start', 'road_end', 'road_inter', 'truck'];
             let missingFields = [];
-            const dateFormat = "DD/MM/YYYY";
-            const durationFormat = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
-            const currentDate = moment().startOf('day');
+            const dateFormat = "DD/MM/YYYY HH:mm";
 
             requiredFields.forEach(field => {
                 if (!req.body[field]) {
                     missingFields.push(field);
                 }
             });
+
             if (missingFields.length > 0) {
                 return res.status(400).json({ error: "Missing fields", missingFields });
             }
 
-            if (!moment(date, dateFormat, true).isValid()) {
-                return res.status(400).json({ error: "Invalid date format. Use DD/MM/YYYY" });
-            }
+            // Create an event
+            const event = await EventService.addEvent({ title, description, date_start, date_end });
 
-            const inputDate = moment(date, dateFormat);
-            if (inputDate.isBefore(currentDate)) {
-                return res.status(400).json({ error: "Date cannot be in the past" });
-            }
 
-            if (!durationFormat.test(duration)) {
-                return res.status(400).json({ error: "Invalid duration format. Use HH:mm" });
-            }
-
-            if (people_needed <= 0) {
-                return res.status(400).json({ error: "Invalid number of people needed. Must be greater than 0" });
-            }
-
-            const formattedDate = moment.utc(date, dateFormat).format('YYYY-MM-DD');
-
-            const maraudeData = { date: formattedDate, duration, id_truck, people_needed };
-            const newMaraude = await MaraudeService.addMaraude(maraudeData);
-            res.status(201).json(newMaraude);
         } catch (error) {
             console.error(error);
             res.status(500).json({ "Error": "Error creating maraude" });
