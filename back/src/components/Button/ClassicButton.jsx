a29ac9aa1026c1308f8ca2739189279287bdeb99
Enzo <enzo.moy94@gmail.com>
2024-05-07 11:18:05 +0200

    New esl rule & button optimization

--- back/src/components/Button/ClassicButton.jsx
+++ back/src/components/Button/ClassicButton.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ClassicButton = ({onClick, children}) => {
     return (
         <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg" onClick={onClick}>{children}</button>
