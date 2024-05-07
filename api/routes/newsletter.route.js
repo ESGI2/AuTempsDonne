e77b00fa66d875be0a50fef4fd39eee318c03301
Karimarf <karimaarfaouim@gmail.com>
2024-05-07 18:23:05 +0200

    Profil main info editable

--- api/routes/newsletter.route.js
+++ api/routes/newsletter.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const NewsletterController = require('../controllers/newsletter.controller');
+const authMiddleware = require("../middlewares/authMiddleware");
+const isAdmin = require("../middlewares/isAdmin");
 
 const router = express.Router();
 
-router.post('/', NewsletterController.sendmail);
+router.post('/', [authMiddleware, isAdmin] , NewsletterController.sendmail);
 
 module.exports = router;
