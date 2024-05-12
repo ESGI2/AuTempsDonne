f5f54fd2a56e8f3b2cfea559eee1a372732fd68e
Enzo <enzo.moy94@gmail.com>
2024-05-12 18:04:25 +0200

    add html maraude map download

--- api/controllers/wasabi.controller.js
+++ api/controllers/wasabi.controller.js
@@ -7,6 +7,11 @@ class WasabiController {
             const fileContent = await WasabiService.getMaraudeFile(id);
 
             if (fileContent) {
+                // Définir les en-têtes de réponse pour spécifier le type de contenu et le nom du fichier
+                res.setHeader('Content-Type', 'text/html');
+                res.setHeader('Content-Disposition', `attachment; filename="maraude_map_${id}.html"`);
+
+                // Envoyer le contenu du fichier en tant que réponse
                 res.status(200).send(fileContent);
             } else {
                 res.status(404).send('File not found');
