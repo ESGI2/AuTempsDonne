b5bb0db5b923fee1a3e36598523821ada61b00fb
Karimarf <karimaarfaouim@gmail.com>
2024-05-12 17:09:19 +0200

    edit profil page

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,15 +3,16 @@ const UserController = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/authMiddleware');
 const isAdmin = require('../middlewares/isAdmin');
 const checkIsResponsible = require('../middlewares/isResponsible');
+const isMe = require('../middlewares/isMe');
 
 const router = express.Router();
 
 router.get('/', [authMiddleware , checkIsResponsible], UserController.getUsers);
 router.delete('/:id', [authMiddleware, isAdmin], UserController.deleteUser);
 router.get('/me', authMiddleware, UserController.getMe);
-router.put('/:id', [authMiddleware, isAdmin], UserController.editUser);
+router.put('/:id', [authMiddleware , isMe], UserController.editUser);
 router.get('/logout', authMiddleware, UserController.logout);
-router.put('/password/:id', [authMiddleware, isAdmin], UserController.editPassword);
+router.put('/password/:id', [authMiddleware, isMe], UserController.editPassword);
 router.get('/volunteers', [authMiddleware, isAdmin], UserController.getVolunteers);
 router.get('/newsletter-subscribers', [authMiddleware, isAdmin], UserController.getNewsletterSubscribers);
 
