97c64b44098aa5b8f6125f51e4294f245396bd8a
Karimarf <karimaarfaouim@gmail.com>
2024-05-13 21:37:05 +0200

    Add boutton don fonctionnelle

--- front/src/component/don/don.jsx
+++ front/src/component/don/don.jsx
@@ -2,33 +2,30 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "./CheckoutForm";
+import { Button } from "reactstrap";
 import materiels from "../../assets/img/materiels.png";
 import aliments from "../../assets/img/aliments.png";
 import hygiene from "../../assets/img/hygiene.png";
 import lang_fr from "../../assets/lang/lang_fr.json";
 import lang_en from "../../assets/lang/lang_en.json";
 
-const stripePromise = loadStripe("pk_test_51PDOe1BlnOamEH8LtvKSKHiDvh842AGkoPQsZzGkxPlFNceJiSGbzr8mRwSXwCTepmeOfRFt7aiMOOCYjKSQyZCq00nElwfvTQ");
+
 
 function Don() {
     const { pathname } = useLocation();
     const lang = pathname.startsWith("/en") ? lang_en : lang_fr;
     const [showModal, setShowModal] = useState(false);
 
-    const handleModalClose = () => {
-        setShowModal(false);
+    const redirectToStripe = () => {
+        window.location.href = "https://buy.stripe.com/cN2g2U1yN0Fz28U288";
     };
+
 
     return (
         <>
-            <div className="mt-5" style={{ marginLeft: "15%", marginRight: "15%" }}>
-                <h1>{lang.don.titre}</h1>
-                <p>{lang.don.paragraphe}</p>
-                <button className="btn btn-primary" onClick={() => setShowModal(true)}>
-                    Faire un don
-                </button>
-            </div>
+            <Button color="primary" className="px-4 , items-center" onClick={redirectToStripe}>
+                Faire un don
+            </Button>
 
             <div className="flex-container mb-5 mt-5" style={{ display: "flex", justifyContent: "space-evenly", marginLeft: "15%", marginRight: "15%", boxShadow: "rgba(0, 0, 0, 5) 0px 4px 20px " }}>
                 <div className="list-container mt-5">
